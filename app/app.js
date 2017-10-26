86b9cda8d743cc004dbe109614aaab44f759b105
Francesc Arpi <farpi@apsl.net>
2017-10-26 16:41:42 +0200

    Tests

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo!')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('port', port)
+
+app.get('/', (req, res) => {
+    res.send('Un altre text')
+})
+
+app.listen(app.get('port'), (err) => {
+    console.log('Server runing on port' + app.get('port'))
+})
